9e4870aeb2e7eb47597f829e5aca5b9ff812f098
Mukeshvenkat <52463730+Mukeshvenkat@users.noreply.github.com>
2024-11-08 10:46:06 +0530

    Ngrok config changes, Upload docs api method added.

--- src/utils/apiRequestUtils.js
+++ src/utils/apiRequestUtils.js
@@ -1,12 +1,13 @@
 import axios from 'axios';
-import { getBaseUrl } from "./constants";
+import { ASYNC_STORAGE_KEYS, getBaseUrl, KYC_PROCESS } from "./constants";
 
 export const ApiRequestUtils = {
     post: async (apiRoute, body, custID = 0) => {
         const token = localStorage.getItem('token');
         const headers = {
             'Content-Type': 'application/json',
-            'token': token
+            'token': token,
+            'ngrok-skip-browser-warning': '69420',
         }
         if (custID != 0) {
             headers['custID'] = custID;
@@ -30,7 +31,8 @@ export const ApiRequestUtils = {
         const token = localStorage.getItem('token');
         const headers = {
             'Content-Type': 'application/json',
-            'token': token
+            'token': token,
+            'ngrok-skip-browser-warning': '69420',
         }
         if (custID != 0) {
             headers['custID'] = custID;
@@ -56,7 +58,8 @@ export const ApiRequestUtils = {
             headers: {
                 'Content-Type': 'application/json',
                 'token': token,
-                'custID': 63
+                'custID': 63,
+                'ngrok-skip-browser-warning': '69420',
             },
             params: params
         });
@@ -76,7 +79,8 @@ export const ApiRequestUtils = {
         const token = localStorage.getItem('token');
         const headers = {
             'Content-Type': 'application/json',
-            'token': token
+            'token': token,
+            'ngrok-skip-browser-warning': '69420',
         }
         if (custID != 0) {
             headers['custID'] = custID;
@@ -102,7 +106,8 @@ export const ApiRequestUtils = {
             headers: {
                 'Content-Type': 'application/json',
                 'token': token,
-                'custID': 63
+                'custID': 63,
+                'ngrok-skip-browser-warning': '69420',
             },
             data: body
         });
@@ -117,6 +122,53 @@ export const ApiRequestUtils = {
             return data;
         }
 
-    }
+    },
+    postDocs: async (apiRoute, body) => {
+        const formData = new FormData();
+
+        if (body.type === KYC_PROCESS.LIVE_PHOTO) {
+            formData.append('type', body.type);
+            formData.append('image1', body);
+            formData.append('driverId', 7);
+        } else {
+            formData.append('id', body.id);
+            formData.append('idNumber', body.idNumber);
+            formData.append('image1', {
+                uri: body.image1.uri,
+                type: body.image1.type,
+                name: body.image1.fileName
+            });
+            formData.append('image2', {
+                uri: body.image2.uri,
+                type: body.image2.type,
+                name: body.image2.fileName
+            });
+            formData.append('extImage1', body.image1.fileName.split('.')[1]);
+            formData.append('fileTypeImage1', body.image1.type);
+            formData.append('extImage2', body.image2.fileName.split('.')[1]);
+            formData.append('fileTypeImage2', body.image2.type);
+            formData.append('mobile', body.mobile);
+            formData.append('name', body.name);
+            formData.append('type', body.type);
+        }
+
+        const token = localStorage.getItem('token');
+        const { data } = await axios.post(getBaseUrl() + apiRoute, formData, {
+            headers: {
+                'Content-Type': 'multipart/form-data',
+                'token': token
+            }
+        });
+        if (!data.success && (data.code === 400 || data.code === 415)) { // Unauthorized request
+            // Alert.alert('Failure', data.message, [{
+            //     style: 'default', onPress: () => {
+            //         // navigation.navigate('Welcome');
+            //     }
+            // }]);
+            return;
+        } else {
+            return data;
+        }
+    },
 };
 
