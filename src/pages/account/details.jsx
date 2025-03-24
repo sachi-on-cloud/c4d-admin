533830d1ef2c5344c39a3d998b9aa309f9d78f71
Mukeshvenkat <52463730+Mukeshvenkat@users.noreply.github.com>
2025-03-24 17:26:06 +0530

    Log & Contract details - code changes added.

--- src/pages/account/details.jsx
+++ src/pages/account/details.jsx
@@ -6,6 +6,8 @@ import { useNavigate, useParams } from "react-router-dom";
 import DocumentsList from '@/components/DocumentsList';
 import { Button } from '@material-tailwind/react';
 import OwnersCabList from '@/components/OwnersCabList';
+import SubscriptionLog from '@/components/SubscriptionLog';
+import DocumentLogs from '@/components/DocumentLogs';
 
 const AccountDetails = ({btnShow = false, noApprove = false }) => {
     const navigate = useNavigate();
@@ -229,6 +231,8 @@ const AccountDetails = ({btnShow = false, noApprove = false }) => {
             </div>
             {accountVal && <OwnersCabList cabsList={accountVal?.Cabs} id={accountVal?.id} ownerName={accountVal?.name} type={accountVal?.type}/>}
             {accountVal && accountVal?.id && <DocumentsList id={accountVal?.id} type={'account'} noApprove={noApprove} />}
+            {accountVal && accountVal?.subscriptionLog && <SubscriptionLog subscriptionlog={accountVal?.subscriptionLog} />}
+            {accountVal && accountVal?.documentLog && <DocumentLogs documentlogs={accountVal?.documentLog} />}
             {!btnShow && 
                 <div className='flex justify-center w-full'>
                     <Button
