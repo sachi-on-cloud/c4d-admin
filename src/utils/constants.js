ecddcf646bd856c6f073d81b214fb40efdd486f4
“Gokulnath” <gokul145308@gmail.com>
2025-05-22 09:13:02 +0530

    Dev - whatsapp template

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -982,12 +982,12 @@ export const whatsappMessage = "Hello, I would like to chat with you!";
 
 export const supportNumber = "+919999999999";
 export const supportEmail = "c4dsupport@texve.com";
-export const supportMessage = `Welcome to C4D.\n Please raise your query!`;
+export const supportMessage = `Welcome to Root Cabs.\n Please raise your query!`;
 
-export const COMPANY_NAME = 'C4D';
+export const COMPANY_NAME = 'Root Cabs';
 export const GST_NUMBER = '1234567890ABC';
 export const GPAY_NUMBER = '8888888888';
-export const GPAY_NAME = 'C4D PRIVATE LIMITED';
+export const GPAY_NAME = 'ROOT CABS PRIVATE LIMITED';
 
 export const USER_ROLE = [
     { id: 'SUPER_USER', role: 'Super user' },
@@ -1105,6 +1105,27 @@ export const WHATSAPP_PAYMENT_REQUEST_TEMPLATE =
     Best regards,
     ${COMPANY_NAME} Team`;
 
+export const WHATSAPP_TRIP_RIDES_COMPLETION_TEMPLATE =
+    `${COMPANY_NAME} Trip Completed
+    Booking ID: \${bookingNumber}
+
+    Hello \${customerName},
+    Thank you for choosing ${COMPANY_NAME}! Your trip with \${driverName} has been successfully completed.
+
+    Trip Summary:
+    • Pickup: \${pickup}
+    • Drop: \${drop}
+    • Start Time: \${startTime}
+    • End Time: \${endTime}
+    • Total Duration: \${totalDuration}
+    • Extra Time: \${extraTime}
+    • Extra Charges: ₹\${extraCharges}
+    • Total Amount Paid: ₹\${totalAmount}
+
+    We hope you had a great journey!
+
+    Warm regards,
+    ${COMPANY_NAME} Team`;
 
 export const WHATSAPP_TRIP_COMPLETION_TEMPLATE =
     `${COMPANY_NAME} Trip Completed
@@ -1124,9 +1145,6 @@ export const WHATSAPP_TRIP_COMPLETION_TEMPLATE =
     • Extra Time: \${extraTime}
     • Extra Charges: ₹\${extraCharges}
     • Total Amount Paid: ₹\${totalAmount}
-
-    Payment Received ✅
-    Payment ID: \${transactionId}
 
     We hope you had a great journey!
 
