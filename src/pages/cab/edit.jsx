db52863ddbcb6edef9b5f273243f86f468cf3243
jagatheshtexve <jagatheshgtexve@gmail.com>
2025-09-16 19:10:19 +0530

    cab packages updated.

--- src/pages/cab/edit.jsx
+++ src/pages/cab/edit.jsx
@@ -120,23 +120,34 @@ const CabEdit = () => {
         });
     };
 
-    const getPackageListDetails = async () => {
-        const data = await ApiRequestUtils.get(API_ROUTES.PACKAGE_CABS_LIST);
-        if (data?.success) {
-            const packageData = data?.data.map(option => {
-                const suffix = option.type === 'Local' ? 'hr' : option.type === 'Outstation' ? 'd' : '';
-                return {
-                    ...option,
-                    period: `${option.period} ${suffix}`, // Append 'hr' or 'd'
-                };
-            });
-            const intercityPackage = orderPackages(packageData.filter(val => val.type === 'Local'), 'Local');
-            const outstationPackage = packageData.filter(val => { return val.type === 'Outstation' && val.period === '1 d' });
-            const carWashPackage = orderPackages(packageData.filter(val => val.type === 'CarWash'), 'CarWash');
-            const ridesPackage = packageData.filter(val => { return val.type == 'Rides' })
-            setPackageDetails([...intercityPackage, ...outstationPackage, ...carWashPackage, ...ridesPackage]);
-        }
-    };
+const getPackageListDetails = async () => {
+    const data = await ApiRequestUtils.get(API_ROUTES.PACKAGE_CABS_LIST);
+    if (data?.success) {
+        const packageData = data?.data.map(option => {
+            const suffix = option.zone === 'Vellore' ? (option.type === 'Local' ? 'hr' : option.type === 'Outstation' ? 'd' : option.type === 'Rides' ? 'Rides' : '') : '';
+            return {
+                ...option,
+                period: `${option.zone === 'Vellore' && option.type !== 'Rides' ? option.period : ''} ${suffix}`.trim(),
+            };
+        });
+
+        // Log only Vellore packages
+        console.log("Vellore Packages:", packageData.filter(val => val.zone === 'Vellore'));
+
+        const intercityPackage = orderPackages(
+            packageData.filter(val => val.zone === 'Vellore' && val.type === 'Local'),'Local');
+        const outstationPackage = packageData.filter(val => val.zone === 'Vellore' && val.type === 'Outstation' && val.period === '1 d');
+        const carWashPackage = orderPackages(packageData.filter(val => val.zone === 'Vellore' && val.type === 'CarWash'),'CarWash');
+        const ridesPackage = packageData.filter(val => val.zone === 'Vellore' && val.type === 'Rides');
+
+        setPackageDetails([
+            ...intercityPackage,
+            ...outstationPackage,
+            ...carWashPackage,
+            ...ridesPackage
+        ]);
+    }
+};
 
     useEffect(() => {
         getPackageListDetails();
