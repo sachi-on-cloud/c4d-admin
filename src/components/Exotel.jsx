fa053ab7c86d0a36d733f8181b3143126a78ab3f
jagatheshtexve <jagatheshgtexve@gmail.com>
2025-10-10 16:35:03 +0530

    exotel - list page completed.

--- src/components/Exotel.jsx
+++ src/components/Exotel.jsx
@@ -1,201 +1,204 @@
-import React, { useEffect, useState, useCallback} from 'react';
-import { Card, CardHeader, CardBody, Typography, Button, Spinner } from "@material-tailwind/react";
+import React, { useEffect, useState } from 'react';
+import { Card, CardHeader, CardBody, Typography, Button, Spinner, Input } from "@material-tailwind/react";
 import { ApiRequestUtils } from "@/utils/apiRequestUtils";
-import { API_ROUTES, ColorStyles } from "@/utils/constants";
-import { useNavigate } from 'react-router-dom';
+import { API_ROUTES } from "@/utils/constants";
 import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
 import moment from "moment";
 
-
-export function ExotelCallsList({ id = 0 }) {
-  const [callList, setcallList] = useState([]);
-  const [searchQuery, setSearchQuery] = useState("");
+export function ExotelCallsList() {
+  const [callList, setCallList] = useState([]);
   const [loading, setLoading] = useState(false);
-  const [pagination, setPagination] = useState({
-    currentPage: 1,
-    totalPages: 1,
-    totalItems: 0,
-    itemsPerPage: 15,
-    search:'',
-  });
 
-  const navigate = useNavigate();
-
-  const callsList = async (page = 1, searchQuery = '', showLoader = false) => {
-    if(showLoader) setLoading(true);
+ 
+  const callsList = async (showLoader = false) => {
+    if (showLoader) setLoading(true);
     try {
-      const data = await ApiRequestUtils.getWithQueryParam(API_ROUTES.GET_AUTO_LIST, {
-        id: id,
-        page: page,
-        limit: pagination.itemsPerPage,
-        search:searchQuery.trim(),
-      });
+      const data = await ApiRequestUtils.getWithQueryParam(API_ROUTES.EXOTEL_CALL_LOGS);
       if (data?.success) {
-        setcallList(data.data || []);
-        setPagination({
-          currentPage: page,
-          totalPages: data?.pagination?.totalPages || 1,
-          totalItems: data?.pagination?.totalItems || 0,
-          itemsPerPage: data?.pagination?.itemsPerPage || 15,
-          search:searchQuery.trim(),
-        });
+        setCallList(data.data || []);
       } else {
         console.error('API request failed:', data?.message);
       }
     } catch (error) {
-      console.error('Error fetching vehicle list:', error);
+      console.error('Error fetching call logs:', error);
     } finally {
-      setLoading(false); 
+      setLoading(false);
     }
   };
- useEffect(()=>{
-    callsList(pagination.currentPage, pagination.search, true);
- },[pagination.currentPage, pagination.search, true])
 
-     const handlePageChange = (page) => {
-        if (page >= 1 && page <= pagination.totalPages) {
-            setPagination((prev) => ({ ...prev, currentPage: page }));
-        }
-    };
-
-  const generatePageButtons = () => {
-    const buttons = [];
-    const maxVisible = 5;
-    let startPage = Math.max(1, pagination.currentPage - Math.floor(maxVisible / 2));
-    let endPage = Math.min(pagination.totalPages, startPage + maxVisible - 1);
-
-    if (endPage - startPage < maxVisible - 1) {
-      startPage = Math.max(1, endPage - maxVisible + 1);
-    }
-
-    for (let i = startPage; i <= endPage; i++) {
-      buttons.push(
-        <Button
-          key={i}
-          size="sm"
-          variant={i === pagination.currentPage ? 'filled' : 'outlined'}
-          className={`mx-1 ${ColorStyles.bgColor} text-white`}
-          onClick={() => handlePageChange(i)}
-        >
-          {i}
-        </Button>
-      );
-    }
-    return buttons;
-  };
+  useEffect(() => {
+    callsList();
+  }, []);
 
 
   return (
     <div className="mb-8 flex flex-col gap-12">
-      <div className="p-4 border border-gray-300 rounded-lg shadow-sm">
-        <div className="relative flex-grow max-w-[500px]">
-          <input
-            type="text"
-            className="w-full px-4 py-2 pl-10 text-sm border border-gray-300 rounded-2xl focus:outline-none focus:ring-2 focus:ring-blue-500"
-            placeholder="Search Phone Number"
-            value={searchQuery}
-            onChange={(e) => {setSearchQuery(e.target.value);                              
-            }}
-          />
-          <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
-            <MagnifyingGlassIcon className="w-5 h-5 text-gray-400" />
-          </div>
-        </div>
+      <div className="p-4 rounded-lg shadow-sm">
       </div>
-
       <Card>
         {callList.length > 0 ? (
           <>
             <CardHeader variant="gradient" color="blue" className="mb-8 p-6 flex justify-between items-center">
-              <Typography variant="h6" color="white">Calls List</Typography>
-              {/* <Typography variant="h6" color="white">{callList.length} vehicles found</Typography> */}
+              <Typography variant="h6" color="white">Call Logs</Typography>
             </CardHeader>
             <CardBody className="overflow-x-scroll px-0 pt-0 pb-2">
               <table className="w-full min-w-[640px] table-auto">
                 <thead>
                   <tr>
-                    {["Name", "Total Calls Offered", "Completed Calls", "Missed Calls", "Average Talk Time","Date"].map((el) => (
+                    {[
+                    // "sid", 
+                    // "Parent Call Sid", 
+                    "Date Created", 
+                    "Date Updated", 
+                    "Account Sid", 
+                    "To", 
+                    "From",
+                    "Phone Number",
+                    // "phone Number Sid",
+                    "Status",
+                    "Start Time",
+                    "End Time",
+                    "Duration",
+                    "Price",
+                    "Direction",
+                    "Answered By",
+                    // "Forwarded From",
+                    // "Caller Name",
+                    // "uri",
+                    // "Custom Field",
+                    // "recordingUrl",
+
+                  ].map((el) => (
                       <th key={el} className="border-b border-blue-gray-50 py-3 px-5 text-left">
-                        
-                          <Typography
-                            variant="small"
-                            className="text-[11px] font-bold uppercase text-black"
-                          >
-                            {el}
-                          </Typography>
+                        <Typography
+                          variant="small"
+                          className="text-[11px] font-bold uppercase text-black"
+                        >
+                          {el}
+                        </Typography>
                       </th>
                     ))}
                   </tr>
                 </thead>
                 <tbody>
                   {loading ? (
-                      <tr>
-                      <td colSpan={9} className="py-3 px-5">
+                    <tr>
+                      <td colSpan={7} className="py-3 px-5">
                         <div className="flex justify-center items-center">
                           <Spinner className="h-12 w-12" />
                         </div>
                       </td>
                     </tr>
                   ) : (
-                  callList.map(
-                    ({ id,name,totalcallsoffered,completedcalls,missedcalls,averagetalktime,created_at  }) => (                      
-                    <tr key={id}>
-                      <td className="py-3 px-5 border-b border-blue-gray-50">
-                        <Typography className="text-xs font-semibold text-blue-gray-600">{name}</Typography>
-                      </td>
-                      <td className="py-3 px-5 border-b border-blue-gray-50">
-                        <div
-                          className="underline cursor-pointer text-blue-600"
-                        >
-                          <Typography className="text-xs font-semibold text-blue-600">{totalcallsoffered}</Typography>
-                        </div>
-                      </td>
-                      <td className="py-3 px-5 border-b border-blue-gray-50">
-                        <Typography className="text-xs font-semibold text-blue-gray-600">{completedcalls}</Typography>
-                      </td>
-                      <td className="py-3 px-5 border-b border-blue-gray-50">
-                        <Typography className="text-xs font-semibold text-blue-gray-600">{missedcalls}</Typography>
-                      </td>
-                        <td className="py-3 px-5 border-b border-blue-gray-50">
-                          <Typography className="text-xs font-semibold text-blue-gray-600">
-                              {averagetalktime}
-                          </Typography>
-                        </td>
-                      <td className="py-3 px-5 border-b border-blue-gray-50">
-                        <Typography className="text-xs font-semibold text-blue-gray-600">
-                          {moment(created_at).format('DD-MM-YYYY / hh:mm A')}
-                        </Typography>
-                      </td>                      
-                    </tr>
-                  )))}
+                    callList.map(
+                      ({ id, 
+                        sid,
+                        parentCallSid,
+                        dateCreated,
+                        dateUpdated,
+                        accountSid,
+                        to,
+                        from,
+                        phoneNumber,
+                        phoneNumberSid,
+                        status,
+                        startTime,
+                        endTime,
+                        duration,
+                        price,
+                        direction,
+                        answeredBy,
+                        forwardedFrom,
+                        callerName,
+                        uri,
+                        customField,
+                        recordingUrl,  
+                      }) => (
+                        <tr key={id}>
+                          {/* <td className="py-3 px-5 border-b border-blue-gray-50">
+                            <Typography className="text-xs font-semibold text-blue-gray-600">{sid}</Typography>
+                          </td> */}
+                          {/* <td className="py-3 px-5 border-b border-blue-gray-50">
+                            <Typography className="text-xs font-semibold text-blue-gray-600">{parentCallSid || '-'}</Typography>
+                          </td> */}
+                          <td className="py-3 px-5 border-b border-blue-gray-50">
+                            <Typography className="text-xs font-semibold text-blue-gray-600">{moment(dateCreated).format("DD-MM-YYYY HH:MM") || '-'}</Typography>
+                          </td>
+                          <td className="py-3 px-5 border-b border-blue-gray-50">
+                            <Typography className="text-xs font-semibold text-blue-gray-600">{moment(dateUpdated).format("DD-MM-YYYY HH:MM") || '-'}</Typography>
+                          </td>
+                          <td className="py-3 px-5 border-b border-blue-gray-50">
+                            <Typography className="text-xs font-semibold text-blue-gray-600">{accountSid|| '-'}</Typography>
+                          </td>
+                          <td className="py-3 px-5 border-b border-blue-gray-50">
+                            <Typography className="text-xs font-semibold text-blue-gray-600">{to|| '-'}</Typography>
+                          </td>
+                          <td className="py-3 px-5 border-b border-blue-gray-50">
+                            <Typography className="text-xs font-semibold text-blue-gray-600">{from|| '-'}</Typography>
+                          </td>
+                          <td className="py-3 px-5 border-b border-blue-gray-50">
+                            <Typography className="text-xs font-semibold text-blue-gray-600">{phoneNumber|| '-'}</Typography>
+                          </td>
+                          {/* <td className="py-3 px-5 border-b border-blue-gray-50">
+                            <Typography className="text-xs font-semibold text-blue-gray-600">{phoneNumberSid || '-'}</Typography>
+                          </td> */}
+                          <td className="py-3 px-5 border-b border-blue-gray-50">
+                            <Typography className="text-xs font-semibold text-blue-gray-600">{status || '-'}</Typography>
+                          </td>
+                          <td className="py-3 px-5 border-b border-blue-gray-50">
+                            <Typography className="text-xs font-semibold text-blue-gray-600">{moment(startTime).format('DD-MM-YYYY HH:MM') || '-'}</Typography>
+                          </td>
+                            <td className="py-3 px-5 border-b border-blue-gray-50">
+                            <Typography className="text-xs font-semibold text-blue-gray-600">{moment(endTime).format('DD-MM-YYYY HH:MM') || '-'}</Typography>
+                          </td>
+                          <td className="py-3 px-5 border-b border-blue-gray-50">
+                            <Typography className="text-xs font-semibold text-blue-gray-600">{duration || '-'}</Typography>
+                          </td>
+                          <td className="py-3 px-5 border-b border-blue-gray-50">
+                            <Typography className="text-xs font-semibold text-blue-gray-600">{price || '-'}</Typography>
+                          </td>
+                          <td className="py-3 px-5 border-b border-blue-gray-50">
+                            <Typography className="text-xs font-semibold text-blue-gray-600">{direction || '-'}</Typography>
+                          </td>
+                          <td className="py-3 px-5 border-b border-blue-gray-50">
+                            <Typography className="text-xs font-semibold text-blue-gray-600">{answeredBy || '-'}</Typography>
+                          </td>
+                          {/* <td className="py-3 px-5 border-b border-blue-gray-50">
+                            <Typography className="text-xs font-semibold text-blue-gray-600">{forwardedFrom || '-'}</Typography>
+                          </td> */}
+                          {/* <td className="py-3 px-5 border-b border-blue-gray-50">
+                            <Typography className="text-xs font-semibold text-blue-gray-600">{callerName || '-'}</Typography>
+                          </td> */}
+                          {/* <td className="py-3 px-5 border-b border-blue-gray-50">
+                            <Typography className="text-xs font-semibold text-blue-gray-600">{uri || '-'}</Typography>
+                          </td> */}
+                          {/* <td className="py-3 px-5 border-b border-blue-gray-50">
+                            <Typography className="text-xs font-semibold text-blue-gray-600">{customField || '-'}</Typography>
+                          </td> */}
+                          {/* <td>
+                          {recordingUrl ? (
+                              <audio controls className="w-full max-w-[200px]">
+                                <source src={recordingUrl} type="audio/mpeg" />
+                                Your browser does not support the audio element.
+                              </audio>
+                            ) : (
+                              <Typography className="text-xs font-semibold text-blue-gray-600">No Audio</Typography>
+                            )}
+                            </td> */}
+                          {/* <td className="py-3 px-5 border-b border-blue-gray-50">
+                            <Typography className="text-xs font-semibold text-blue-gray-600">{}</Typography>
+                          </td> */}
+                        </tr>
+                      )
+                    )
+                  )}
                 </tbody>
               </table>
-              <div className="flex items-center justify-center mt-4">
-                <Button
-                  size="sm"
-                  variant="text"
-                  disabled={pagination.currentPage === 1}
-                  onClick={() => handlePageChange(pagination.currentPage - 1)}
-                  className="mx-1"
-                >
-                  {'<'}
-                </Button>
-                {generatePageButtons()}
-                <Button
-                  size="sm"
-                  variant="text"
-                  disabled={pagination.currentPage === pagination.totalPages}
-                  onClick={() => handlePageChange(pagination.currentPage + 1)}
-                  className="mx-1"
-                >
-                  {'>'}
-                </Button>
-              </div>
             </CardBody>
           </>
         ) : (
           <CardHeader variant="gradient" color="blue" className="mb-8 p-6">
-            <Typography variant="h6" color="white">No Calls Available</Typography>
+            <Typography variant="h6" color="white">No Call Records Available</Typography>
           </CardHeader>
         )}
       </Card>
