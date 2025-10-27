507cb7a295406b9f0a6e5ea5e189ec4cc98088a7
jagatheshtexve <jagatheshgtexve@gmail.com>
2025-10-27 21:43:23 +0530

    prod - Remove unused status display logic from TextBoxWithList component

--- src/components/BookingNotes.jsx
+++ src/components/BookingNotes.jsx
@@ -324,23 +324,6 @@ const TextBoxWithList = ({addNotes, notesData, bookingId }) => {
                   </>
                 )}
                   </span>
-                  <span
-                    className={`inline-flex items-center px-2 py-0.5 rounded-full text-xs font-medium 
-                      ${log.type === "booking"
-                      ? log?.new_status === 'Confirmed'
-                        ? 'bg-green-100 text-green-800'
-                        : log?.new_status === 'Cancelled'
-                        ? 'bg-red-100 text-red-800'
-                        : 'bg-gray-100 text-gray-800'
-                        : (log?.newStatus === "FOLLOWUP" || log?.newStatus === "NONE")
-                        ? "bg-yellow-100 text-yellow-800"
-                        : log?.newStatus === "FOLLOWUP_COMPLETED"
-                        ? "bg-green-100 text-green-800"
-                        : "bg-gray-100 text-gray-800"
-                    }`}
-                  >
-                    {/* {log?.new_status || 'N/A'} */}
-                  </span>
                 </div>
               </li>
             ))}
