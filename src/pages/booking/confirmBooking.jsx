810ef4aa1c4d65ea8d85ffa65560cbbac7f8401a
Arunaja30 <arunaja2001@gmail.com>
2025-10-29 12:27:57 +0530

    update follow-up functionality in booking confirmation page

--- src/pages/booking/confirmBooking.jsx
+++ src/pages/booking/confirmBooking.jsx
@@ -289,7 +289,7 @@ const ConfirmBooking = (props) => {
                         Back
                     </Button> */}
 
-                {bookingDetails.status === "QUOTED" && (
+              {bookingDetails.status === "QUOTED" && bookingDetails.followup !== "FOLLOWUP" && (
                     <Button
                         color="green"
                         variant="outlined"
