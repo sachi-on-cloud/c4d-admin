541e612249b3ef27de97d0368fe4c233b21e2b70
Arunaja30 <arunaja2001@gmail.com>
2025-11-17 17:54:15 +0530

    update Complete status filter

--- src/pages/booking/list.jsx
+++ src/pages/booking/list.jsx
@@ -229,7 +229,7 @@ const handleTabChange = (value) => {
     try {
         const filterType = {
             type: activeTab,
-            status: statusFilter.includes('COMPLETED') ? ['ENDED'] : statusFilter.filter(s => s !== 'COMPLETED'),
+            status: statusFilter,
             source: sourceFilter,
             tripCoordinator: tripCoordinatorFilter,
             tripStatus: statusFilter.includes('COMPLETED') ? true : statusFilter.includes('ENDED') ? false : undefined,
@@ -528,7 +528,7 @@ const handleTabChange = (value) => {
         try {
             const filterType = {
                 type: activeTab,
-                status: statusFilterParam.includes('COMPLETED') ? ['ENDED'] : statusFilterParam.filter(s => s !== 'COMPLETED'),
+                status: statusFilterParam,
                 source: sourceFilterParam,
                 tripCoordinator: tripCoordinatorFilterParam,
                 tripStatus: statusFilterParam.includes('COMPLETED') ? true : statusFilterParam.includes('ENDED') ? false : undefined,
