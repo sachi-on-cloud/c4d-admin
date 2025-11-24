8dd3d9d2830e20d241e7e336ced286f520e39d54
Arunaja30 <arunaja2001@gmail.com>
2025-11-24 15:26:34 +0530

    add Analytics permission to ROLE_PERMISSIONS and PERMISSION_OPTIONS

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1074,7 +1074,7 @@ export const USER_ROLE = [
 
 // Role-based permissions
 export const ROLE_PERMISSIONS = {
-    'SUPER_USER': ['Home', "Calls", 'All bookings', 'Customers', 'Vendors', 'Trip Master','Finance', 'Document verification','Marketing', 'Users'],
+    'SUPER_USER': ['Home', "Calls", 'All bookings', 'Customers', 'Vendors', 'Trip Master','Finance', 'Document verification','Marketing', 'Users','Analytics'],
     'SALES': ['Home', 'All bookings', 'Customers', 'Vendors', 'Document verification'],
     'SUPPORT': ['Home', 'All bookings', 'Customers', 'Vendors'],
     'FINANCE': ['Home', 'All bookings', 'Customers', 'Vendors', 'Finance', 'Document verification'],
@@ -1083,6 +1083,7 @@ export const ROLE_PERMISSIONS = {
 export const PERMISSION_OPTIONS = [
     { name: 'Home', id: 'Home' },
     { name: 'Calls', id: 'Calls'},
+     { name: 'Analytics', id: 'Analytics'},
     { name: 'All bookings', id: 'All bookings' },
     { name: 'Customers', id: 'Customers' },
     { name: 'Vendors', id: 'Vendors' },
