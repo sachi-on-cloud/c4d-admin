5a3422db0337d6fddb9032b5858b721fe8235b1d
jagatheshtexve <jagatheshgtexve@gmail.com>
2026-02-19 22:09:38 +0530

    Refactor hourly package pricing logic to correctly calculate and display per KM rate based on service type

--- src/pages/booking/confirmBooking.jsx
+++ src/pages/booking/confirmBooking.jsx
@@ -735,7 +735,7 @@ const hasAdditionalCharges = Object.values(additionalCharges || {}).some((value)
         (
             bookingDetails?.serviceType === 'AUTO' ||
             bookingDetails?.serviceType === 'RIDES' ||
-            isHourlyShowingPrice(bookingDetails) ||
+            // isHourlyShowingPrice(bookingDetails) ||
             isOutstationBooking(bookingDetails) ||
             isDropTaxiBooking(bookingDetails)
         ) &&
@@ -745,6 +745,7 @@ const hasAdditionalCharges = Object.values(additionalCharges || {}).some((value)
             shouldShowQuotePricing(bookingDetails)
         );
     const hourlyPackageBaseFare = isHourlyShowingPrice(bookingDetails)
+        // ? Number(bookingDetails?.estimatedFareBreakdown?.distanceFare?.rate || 0)
         ? Number(
             bookingDetails?.carType === "Sedan"
                 ? bookingDetails?.Package?.baseFareSedan
@@ -755,6 +756,19 @@ const hasAdditionalCharges = Object.values(additionalCharges || {}).some((value)
                         : bookingDetails?.Package?.baseFare || 0
         )
         : 0;
+const hourlyPackagePerKm = isHourlyShowingPrice(bookingDetails)
+  ? Number(bookingDetails?.estimatedFareBreakdown?.distanceFare?.rate || 0)
+  : Number(
+      bookingDetails?.carType === "Sedan"
+        ? bookingDetails?.Package?.kilometerPriceSedan
+        : bookingDetails?.carType === "MUV"
+          ? bookingDetails?.Package?.kilometerPriceMVP
+          : bookingDetails?.carType === "SUV"
+            ? bookingDetails?.Package?.kilometerPriceSuv
+            : bookingDetails?.Package?.kilometerPrice || 0
+    );
+
+
     return (
         <div className="container mx-auto px-2 md:px-6">
             {bookingDetails && (
@@ -1365,6 +1379,12 @@ const hasAdditionalCharges = Object.values(additionalCharges || {}).some((value)
                                     <span className="text-gray-900 font-medium">₹ {baseFareToShow}</span>
                                 </div>
                             )}
+                                    {!!hourlyPackagePerKm && (
+                                        <div className="flex gap-2">
+                                            <span className="text-gray-500 font-semibold">Per KM Rate:</span>
+                                            <span className="text-gray-900 font-medium">₹ {hourlyPackagePerKm}</span>
+                                        </div>
+                                    )}
                             
                               {shouldShowPerKmRate && (
                              <div className="flex flex-col-2 gap-2">
