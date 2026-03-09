9d1c770c3176eb48ce70f00f7c9fcf47c02ccbe3
jagatheshtexve <jagatheshgtexve@gmail.com>
2026-03-09 22:46:23 +0530

    user permission auto list updated in the constants files.

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1115,10 +1115,10 @@ export const USER_ROLE = [
 
 // Role-based permissions for main modules (side/top nav)
 export const ROLE_PERMISSIONS = {
-    'SUPER_USER': ['Home','Support','Driver Ops',"Calls", 'All bookings', 'Customers', 'Vendors', 'Trip Master','Finance', 'Document verification','Marketing', 'Users','Driver Engagement'], //'Autos'
-    'SALES': ['Home','Support','Driver Ops','All bookings', 'Customers', 'Vendors', 'Document verification'], // ,'Autos'
-    'SUPPORT': ['Home','Support','Calls','All bookings', 'Customers', 'Vendors'], //,'Autos'
-    'FINANCE': ['Home','Support','Driver Ops','All bookings', 'Customers', 'Vendors', 'Finance','Document verification'], //,'Autos'
+    'SUPER_USER': ['Home','Support','Driver Ops',"Calls", 'All bookings', 'Customers', 'Vendors', 'Trip Master','Finance', 'Document verification','Marketing', 'Users','Driver Engagement','Autos'],
+    'SALES': ['Home','Support','Driver Ops','All bookings', 'Customers', 'Vendors', 'Document verification','Autos'],
+    'SUPPORT': ['Home','Support','Calls','All bookings', 'Customers', 'Vendors','Autos'],
+    'FINANCE': ['Home','Support','Driver Ops','All bookings', 'Customers', 'Vendors', 'Finance','Document verification','Autos'],
 };
 
 export const PERMISSION_GROUPS = {
@@ -1172,7 +1172,7 @@ export const PERMISSION_OPTIONS = [
     { name: 'Support', id: 'Support' },
     { name: 'Driver Ops', id: 'Driver Ops' },
     { name: 'Calls', id: 'Calls'},
-    // { name: 'Autos', id: 'Autos'},
+    { name: 'Autos', id: 'Autos'},
     { name: 'Driver Engagement', id: 'Driver Engagement'},
     { name: 'All bookings', id: 'All bookings' },
     { name: 'Customers', id: 'Customers' },
